49a92814310a2e1455b32e0634ed2c09df887ee6
Joel Crawford <joelcrawford30@gmail.com>
2023-09-30 14:10:45 +0300

    BFS with expanded and unexpanded

--- question5/breadth-first-search-for-tree.py
+++ question5/breadth-first-search-for-tree.py
@@ -17,9 +17,15 @@ def bfs(graph, start):
     visited = set()
     queue = deque([start])
     path = []
+    
+    # To store the order in which states are expanded
+    expanded_order = []  
 
     while queue:
         node = queue.popleft()
+         # Record expanded states
+        expanded_order.append(node) 
+
         if node not in visited:
             visited.add(node)
             path.append(node)
@@ -28,8 +34,7 @@ def bfs(graph, start):
                 if neighbor not in visited:
                     queue.append(neighbor)
 
-    return path
-
+    return path, expanded_order
 
 if __name__ == "__main__":
     g = Graph()
@@ -42,20 +47,12 @@ if __name__ == "__main__":
     g.add_edge("C", "D")
     g.add_edge("C", "G")
     g.add_edge("D", "G")
-    
-    # g.add_edge("S", "A")
-    # g.add_edge("S", "B")
-    # g.add_edge("A", "B")
-    # g.add_edge("A", "C")
-    # g.add_edge("B", "C")
-    # g.add_edge("C", "D")
-    # g.add_edge("C", "G")
-    # g.add_edge("D", "G")
-    # g.add_edge("B", "C")
-    # g.add_edge("C", "D")
-    # g.add_edge("C", "G")
-    # g.add_edge("D", "G")
 
-    print("BFS traversal starting from S:")
-    bfs_path = bfs(g.graph, "S")
-    print(" -> ".join(bfs_path))
+    start_node = "S"
+
+    bfs_path, expanded_states = bfs(g.graph, start_node)
+
+    print("BFS path starting from", start_node, ":", " -> ".join(bfs_path))
+    print("Expanded states:", " -> ".join(expanded_states))
+    unexpanded_states = set(g.graph.keys()) - set(expanded_states)
+    print("Unexpanded states:", " -> ".join(sorted(unexpanded_states)))
